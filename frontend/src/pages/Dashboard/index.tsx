e685f396b0073bca8a526f1562bef8a8a53cb69f
Crummorn <Igorcaze@gmail.com>
2021-01-22 15:41:51 -0300

    F-Feat: Add Page Dashboard.

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../../components/Navbar';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <Header />
+
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  </>
+);
+
+export default Dashboard;
---
+++ frontend/src/pages/Dashboard/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  height: 100vh;
+  padding: 0 10px;
+
+  @media (min-width: 768px) {
+    padding: 10px 40px;
+  }
+`;
