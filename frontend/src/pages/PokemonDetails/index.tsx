b3e59f493f6e75690042e3fb80a6940ec9d10130
Crummorn <Igorcaze@gmail.com>
2021-01-24 12:33:11 -0300

    F-Feat: Add Page PokemonDetails.

---
+++ frontend/src/pages/PokemonDetails/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const PokemonDetails: React.FC = () => (
+  <Container>
+    <h1>Pokemon Details</h1>
+  </Container>
+);
+
+export default PokemonDetails;
