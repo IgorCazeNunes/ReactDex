293ddba5c2d22cef881b398fa3c3697b1ad3f12c
Crummorn <Igorcaze@gmail.com>
2021-01-24 12:34:04 -0300

    F-Chore: Add Page PokemonDetails ao sistema de rotas.

--- frontend/src/Routes/index.tsx
+++ frontend/src/Routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
+import PokemonDetails from '../pages/PokemonDetails';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
+    <Route path="/details/:id" component={PokemonDetails} />
   </Switch>
 );
 
