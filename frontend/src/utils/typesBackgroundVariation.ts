f6c336db1d75969761f5241e7730989e48404b11
Crummorn <Igorcaze@gmail.com>
2021-01-25 15:52:04 -0300

    F-Feat: Add função para retornar todos os tipos com cores pre-definidas.

---
+++ frontend/src/utils/typesBackgroundVariation.ts
@@ -0,0 +1,57 @@
+const typesBackgroundVariation = {
+  /** background: #a8a878 */
+  normal: '#a8a878',
+
+  /** background: #f08030 */
+  fire: '#f08030',
+
+  /** background: #c0302b */
+  fighting: '#c0302b',
+
+  /** background: #6890f0 */
+  water: '#6890f0',
+
+  /** background: #a890f0 */
+  flying: '#a890f0',
+
+  /** background: #75c850 */
+  grass: '#78c850',
+
+  /** background: #a040a0 */
+  poison: '#a040a0',
+
+  /** background: #f8d030 */
+  electric: '#f8d030',
+
+  /** background: #e0c068 */
+  ground: '#e0c068',
+
+  /** background: #f85888 */
+  psychic: '#f85888',
+
+  /** background: #b8a03b */
+  rock: '#b8a03b',
+
+  /** background: #98d8d8 */
+  ice: '#98d8d8',
+
+  /** background: #a8b820 */
+  bug: '#a8b820',
+
+  /** background: #7038f8 */
+  dragon: '#7038f8',
+
+  /** background: #705898 */
+  ghost: '#705898',
+
+  /** background: #705848 */
+  dark: '#705848',
+
+  /** background: #b8b8d0 */
+  steel: '#b8b8d0',
+
+  /** background: #ee99ac */
+  fairy: '#ee99ac',
+};
+
+export default typesBackgroundVariation;
