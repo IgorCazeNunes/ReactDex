7bcea41b1e5b7727d9f394052ec4763ac557ba70
Crummorn <Igorcaze@gmail.com>
2021-01-26 15:31:22 -0300

    F-Feat: Add estrutura da Page PokemonDetails.

--- frontend/src/pages/PokemonDetails/index.tsx
+++ frontend/src/pages/PokemonDetails/index.tsx
@@ -1,10 +1,125 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
 
-import { Container } from './styles';
+import Header from '../../components/Header';
+import TypeBadge from '../../components/TypeBadge';
+
+import CharizardImage from '../../assets/charizard-render.png';
+
+import {
+  Container,
+  Content,
+  ContainerHeader,
+  PokemonDescription,
+  PokemonStats,
+  PokemonStat,
+} from './styles';
 
 const PokemonDetails: React.FC = () => (
   <Container>
-    <h1>Pokemon Details</h1>
+    <Header />
+
+    <Content>
+      <ContainerHeader>
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          <span>Back</span>
+        </Link>
+
+        <span>003 - Charizard</span>
+      </ContainerHeader>
+
+      <PokemonDescription>
+        <aside>
+          <img src={CharizardImage} alt="Charizard" />
+
+          <ul>
+            <li>
+              <TypeBadge type="fire" />
+            </li>
+            <li>
+              <TypeBadge type="dragon" />
+            </li>
+          </ul>
+        </aside>
+
+        <article>
+          <h2>About</h2>
+
+          <ul>
+            <li>
+              <h3>Titulo</h3>
+              <p>Conteudo</p>
+            </li>
+
+            <hr />
+
+            <li>
+              <div>
+                <h3>Titulo</h3>
+                <p>Conteudo</p>
+              </div>
+
+              <div>
+                <h3>Titulo</h3>
+                <p>Conteudo</p>
+              </div>
+            </li>
+
+            <hr />
+
+            <li>
+              <h3>Titulo</h3>
+              <p>Conteudo</p>
+            </li>
+
+            <hr />
+
+            <li>
+              <h3>Titulo</h3>
+              <p>Conteudo</p>
+            </li>
+          </ul>
+        </article>
+      </PokemonDescription>
+
+      <PokemonStats>
+        <h2>Stats</h2>
+
+        <ul>
+          <PokemonStat>
+            <h3>hp</h3>
+            <span>123</span>
+          </PokemonStat>
+
+          <PokemonStat>
+            <h3>speed</h3>
+            <span>123</span>
+          </PokemonStat>
+
+          <PokemonStat>
+            <h3>atk</h3>
+            <span>123</span>
+          </PokemonStat>
+
+          <PokemonStat>
+            <h3>sp. atk</h3>
+            <span>123</span>
+          </PokemonStat>
+
+          <PokemonStat>
+            <h3>def</h3>
+            <span>123</span>
+          </PokemonStat>
+
+          <PokemonStat>
+            <h3>sp. def</h3>
+            <span>123</span>
+          </PokemonStat>
+        </ul>
+      </PokemonStats>
+    </Content>
   </Container>
 );
 
--- frontend/src/pages/PokemonDetails/styles.ts
+++ frontend/src/pages/PokemonDetails/styles.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components';
 
-export const Container = styled.main``;
+import mediaBreak from '../../utils/mediaQueryBreakPoints';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Content = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  height: 100vh;
+  max-width: 1360px;
+
+  background: #ffffff;
+  border-radius: 10px;
+
+  padding: 20px;
+  margin: 10px;
+
+  @media (${mediaBreak.mdMin}) {
+    margin: 20px 40px;
+  }
+
+  @media (${mediaBreak.xlMin}) {
+    margin: 20px 80px;
+  }
+`;
+
+export const ContainerHeader = styled.h1``;
+
+export const PokemonDescription = styled.section``;
+
+export const PokemonStats = styled.section``;
+
+export const PokemonStat = styled.li``;
