6c9588c854167b7aa03f8333028e13f1d2b3ab23
Crummorn <Igorcaze@gmail.com>
2021-01-28 12:41:20 -0300

    F-Chore: Adicionado interfaces necessarias para requisições.

--- frontend/src/pages/PokemonDetails/index.tsx
+++ frontend/src/pages/PokemonDetails/index.tsx
@@ -15,117 +15,236 @@ import {
   PokemonStats,
 } from './styles';
 
-const PokemonDetails: React.FC = () => (
-  <Container>
-    <Header />
+interface TypeData {
+  slot: number;
+  type: {
+    name:
+      | 'normal'
+      | 'fire'
+      | 'fighting'
+      | 'water'
+      | 'flying'
+      | 'grass'
+      | 'poison'
+      | 'electric'
+      | 'ground'
+      | 'psychic'
+      | 'rock'
+      | 'ice'
+      | 'bug'
+      | 'dragon'
+      | 'ghost'
+      | 'dark'
+      | 'steel'
+      | 'fairy';
+  };
+}
 
-    <Content>
-      <ContainerHeader>
-        <Link to="/">
-          <FiChevronLeft size={16} />
-          <span>Back</span>
-        </Link>
+interface PokemonRequest {
+  name: string;
 
-        <span>003 Charizard</span>
-      </ContainerHeader>
+  height: number;
 
-      <PokemonDescription>
-        <PokemonAside>
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"
-            alt="Charizard"
-          />
+  weight: number;
 
-          <ul>
-            <li>
-              <TypeBadge type="fire" />
-            </li>
+  sprites: {
+    other: {
+      'official-artwork': {
+        // eslint-disable-next-line camelcase
+        front_default: string;
+      };
+    };
+  };
 
-            <li>
-              <TypeBadge type="dragon" />
-            </li>
-          </ul>
-        </PokemonAside>
+  types: Array<TypeData>;
 
-        <div>
-          <PokemonAbout>
-            <h2>About</h2>
+  stats: Array<{
+    // eslint-disable-next-line camelcase
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }>;
+}
+
+interface PokemonSpeciesRequest {
+  habitat: {
+    name: string;
+  };
+
+  // eslint-disable-next-line camelcase
+  egg_groups: Array<{
+    name: string;
+  }>;
+
+  // eslint-disable-next-line camelcase
+  flavor_text_entries: Array<{
+    // eslint-disable-next-line camelcase
+    flavor_text: string;
+
+    language: {
+      name: string;
+    };
+
+    version: {
+      name: string;
+    };
+  }>;
+}
+
+interface PokemonData {
+  id: string;
+  name: string;
+  height: number;
+  weight: number;
+  sprites: {
+    other: {
+      'official-artwork': {
+        // eslint-disable-next-line camelcase
+        front_default: string;
+      };
+    };
+  };
+  habitat: {
+    name: string;
+  };
+  types: Array<TypeData>;
+  stats: Array<{
+    // eslint-disable-next-line camelcase
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }>;
+  // eslint-disable-next-line camelcase
+  egg_groups: Array<{
+    name: string;
+  }>;
+  // eslint-disable-next-line camelcase
+  flavor_text_entries: Array<{
+    // eslint-disable-next-line camelcase
+    flavor_text: string;
+    language: {
+      name: string;
+    };
+    version: {
+      name: string;
+    };
+  }>;
+}
+
+const PokemonDetails: React.FC = () => {
+  return (
+    <Container>
+      <Header />
+
+      <Content>
+        <ContainerHeader>
+          <Link to="/">
+            <FiChevronLeft size={16} />
+            <span>Back</span>
+          </Link>
+
+          <span>003 Charizard</span>
+        </ContainerHeader>
+
+        <PokemonDescription>
+          <PokemonAside>
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"
+              alt="Charizard"
+            />
 
             <ul>
               <li>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-                  ratione maxime atque fugit at enim et rerum eius accusantium,
-                  neque soluta quisquam! Eum voluptatibus est iste animi,
-                  officiis molestias minus!
-                </p>
+                <TypeBadge type="fire" />
               </li>
 
               <li>
-                <div>
-                  <h3>Height:</h3>
-                  <span>1.7m</span>
-                </div>
-
-                <div>
-                  <h3>Weight:</h3>
-                  <span>905kg</span>
-                </div>
-              </li>
-
-              <li>
-                <div>
-                  <h3>Habitat:</h3>
-                  <span>Mountain</span>
-                </div>
-
-                <div>
-                  <h3>Egg Group:</h3>
-                  <span>Monster Dragon</span>
-                </div>
+                <TypeBadge type="dragon" />
               </li>
             </ul>
-          </PokemonAbout>
+          </PokemonAside>
 
-          <PokemonStats>
-            <h2>Stats</h2>
+          <div>
+            <PokemonAbout>
+              <h2>About</h2>
 
-            <ul>
-              <li>
-                <h3>hp</h3>
-                <span>123</span>
-              </li>
+              <ul>
+                <li>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iusto ratione maxime atque fugit at enim et rerum eius
+                    accusantium, neque soluta quisquam! Eum voluptatibus est
+                    iste animi, officiis molestias minus!
+                  </p>
+                </li>
 
-              <li>
-                <h3>attack</h3>
-                <span> 123</span>
-              </li>
+                <li>
+                  <div>
+                    <h3>Height:</h3>
+                    <span>1.7m</span>
+                  </div>
 
-              <li>
-                <h3>defense</h3>
-                <span>123</span>
-              </li>
+                  <div>
+                    <h3>Weight:</h3>
+                    <span>905kg</span>
+                  </div>
+                </li>
 
-              <li>
-                <h3>sp. attack</h3>
-                <span>123</span>
-              </li>
+                <li>
+                  <div>
+                    <h3>Habitat:</h3>
+                    <span>Mountain</span>
+                  </div>
 
-              <li>
-                <h3>sp. defense</h3>
-                <span>123</span>
-              </li>
+                  <div>
+                    <h3>Egg Group:</h3>
+                    <span>Monster Dragon</span>
+                  </div>
+                </li>
+              </ul>
+            </PokemonAbout>
 
-              <li>
-                <h3>speed</h3>
-                <span>123</span>
-              </li>
-            </ul>
-          </PokemonStats>
-        </div>
-      </PokemonDescription>
-    </Content>
-  </Container>
-);
+            <PokemonStats>
+              <h2>Stats</h2>
 
+              <ul>
+                <li>
+                  <h3>hp</h3>
+                  <span>123</span>
+                </li>
+
+                <li>
+                  <h3>attack</h3>
+                  <span> 123</span>
+                </li>
+
+                <li>
+                  <h3>defense</h3>
+                  <span>123</span>
+                </li>
+
+                <li>
+                  <h3>sp. attack</h3>
+                  <span>123</span>
+                </li>
+
+                <li>
+                  <h3>sp. defense</h3>
+                  <span>123</span>
+                </li>
+
+                <li>
+                  <h3>speed</h3>
+                  <span>123</span>
+                </li>
+              </ul>
+            </PokemonStats>
+          </div>
+        </PokemonDescription>
+      </Content>
+    </Container>
+  );
+};
 export default PokemonDetails;
