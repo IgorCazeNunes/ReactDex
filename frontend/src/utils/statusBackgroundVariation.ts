e4a015858184175204cc57fa9fe3ae7efc164e67
Crummorn <igorcaze@gmail.com>
2021-03-03 14:20:46 -0300

    Feat: Função para auxiliar a status bar.

---
+++ frontend/src/utils/statusBackgroundVariation.ts
@@ -0,0 +1,21 @@
+const statusBackgroundVariation = {
+  /** background: #ff0000 */
+  hp: '#ff0000',
+
+  /** background: #f08030 */
+  attack: '#f08030',
+
+  /** background: #f8d030 */
+  defense: '#f8d030',
+
+  /** background: #6890f0 */
+  special_attack: '#6890f0',
+
+  /** background: #78c850 */
+  special_defense: '#78c850',
+
+  /** background: #f85888 */
+  speed: '#f85888',
+};
+
+export default statusBackgroundVariation;
