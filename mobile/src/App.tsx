0c331e115d43774a2823dfe8adf47cac86c79a3d
Igor Cazé <igorcaze@gmail.com>
2021-04-17 16:02:00 -0300

    M-Feat: Configurado rotas principais.

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,28 +1,19 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
     return (
         <>
-            <StatusBar barStyle="light-content" backgroundColor="#e3350d" />
+            <NavigationContainer>
+                <StatusBar barStyle="light-content" backgroundColor="#e3350d" />
 
-            <View
-                style={{
-                    flex: 1,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    backgroundColor: '#e2e2e2',
-                }}
-            >
-                <Text
-                    style={{
-                        fontSize: 40,
-                        color: '#e3350d',
-                    }}
-                >
-                    ReactDex
-                </Text>
-            </View>
+                <Routes />
+            </NavigationContainer>
         </>
     );
 };
---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Dashboard from '../pages/Dashboard';
+import PokemonDetails from '../pages/PokemonDetails';
+
+const App = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+    <App.Navigator
+        screenOptions={{
+            headerShown: false,
+            cardStyle: { backgroundColor: '#e2e2e2' },
+        }}
+    >
+        <App.Screen name="Dashboard" component={Dashboard} />
+        <App.Screen name="PokemonDetails" component={PokemonDetails} />
+    </App.Navigator>
+);
+
+export default AppRoutes;
