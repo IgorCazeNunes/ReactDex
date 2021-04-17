9bcd4de39a48b8a8dd7eab8cf6a1f83601b278ff
Igor Cazé <igorcaze@gmail.com>
2021-04-17 16:00:52 -0300

    M-Chore: Iniciado estrutura das paginas principais.

---
+++ mobile/src/pages/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container, Content } from './styles';
+
+const Dashboard: React.FC = () => (
+    <Container>
+        <Content>
+            <Text>Dashboard</Text>
+        </Content>
+    </Container>
+);
+
+export default Dashboard;
---
+++ mobile/src/pages/PokemonDetails/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container, Content } from './styles';
+
+const PokemonDetails: React.FC = () => (
+    <Container>
+        <Content>
+            <Text>PokemonDetails</Text>
+        </Content>
+    </Container>
+);
+
+export default PokemonDetails;
