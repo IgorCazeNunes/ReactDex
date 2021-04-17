1119ceb79362a95a1c81a6def691f3a3249d2468
Igor Cazé <igorcaze@gmail.com>
2021-04-17 15:05:25 -0300

    M-Chore: Configurado StatusBar.

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 
-const App: React.FC = () => (
-    <View
-        style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-        }}
-    >
-        <Text
-            style={{
-                fontSize: 40,
-                color: '#ffffff',
-            }}
-        >
-            ReactDex
-        </Text>
-    </View>
-);
+const App: React.FC = () => {
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#e3350d" />
+
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#e2e2e2',
+                }}
+            >
+                <Text
+                    style={{
+                        fontSize: 40,
+                        color: '#e3350d',
+                    }}
+                >
+                    ReactDex
+                </Text>
+            </View>
+        </>
+    );
+};
 
 export default App;
